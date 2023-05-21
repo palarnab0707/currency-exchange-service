7ad4581c0399478c9b9ac6aaa189026c9a940de8
palarnab0707 <79499723+palarnab0707@users.noreply.github.com>
2023-05-21 23:44:32 +0530

    First Commit

---
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -0,0 +1,34 @@
+package com.in28minutes.microservices.currencyexchangeservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+
+@RestController
+public class CircuitBreakerController {
+	
+	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
+	@GetMapping("/sample-api")
+	//@Retry(name = "sample-api", fallbackMethod = "fallBackMethod")
+	//@CircuitBreaker(name = "sample-api", fallbackMethod = "fallBackMethod")
+	//@Bulkhead(name = "sample-api")
+	@RateLimiter(name = "sample-api")
+	public String sampleApi() {
+		
+//		logger.info("Sample Api called");
+//		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/dummy-api", String.class);
+//		
+		return "sample-api";
+	}
+	
+	public String fallBackMethod(Exception ex) {
+		return "fall-back-method";
+	}
+}
